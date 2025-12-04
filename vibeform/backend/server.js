1c304bb5cca0c8edddf10262fc256fcbbd266ce7
Vivekbhai Patel <VivekPatel8433@gmail.com>
2025-12-03 18:03:24 -0700

    Created Folders, structures and installed dependencies.

---
+++ vibeform/backend/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv"
+
+dotenv.config();
+const PORT = process.env.PORT || 5000 ; 
+
+const app = express();
+app.use(cors()); 
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("VibeForm API running");
+});
+
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
