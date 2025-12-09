6856c943b89dd61aeac9194637d5c050c1fb2769
Vivekbhai Patel <VivekPatel8433@gmail.com>
2025-12-09 14:46:00 -0700

    Updated the backend routes, cors for deployment

--- vibeform/backend/server.js
+++ vibeform/backend/server.js
@@ -12,7 +12,8 @@ const app = express();
 const PORT = process.env.PORT || 5000 ; 
 
 app.use(cors({
-  origin: "http://localhost:5173" 
+  // origin: "http://localhost:5173" Local
+    origin: "https:vibe-form-one.vercel.app"
 }));
 
 app.use(cors()); 
